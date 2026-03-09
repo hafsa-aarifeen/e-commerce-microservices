815a13a3a4283f08c6e72d2dc28d9acbc469df6f
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-03-09 16:47:56 +0530

    feat(order-service): integrate Clerk authentication and add test endpoint (Fastify.js Microservice Authentication)

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -4,16 +4,25 @@ const TestPage = async () => {
   const { getToken } = await auth();
   const token = await getToken();
 
-  console.log(token);
-  const res = await fetch("http://localhost:8000/test", {
+  const resProduct = await fetch("http://localhost:8000/test", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
-  const data = await res.json();
+  const dataProduct = await resProduct.json();
 
-  console.log(data);
+  console.log(dataProduct);
+
+  const resOrder = await fetch("http://localhost:8001/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const dataOrder = await resOrder.json();
+
+  console.log(dataOrder);
   return <div className="test-page">TestPage</div>;
 };
 
--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,12 +1,25 @@
 import Fastify from "fastify";
+import { clerkPlugin, getAuth } from "@clerk/fastify";
 
 const fastify = Fastify();
+
+fastify.register(clerkPlugin);
 
 fastify.get("/health", (request, reply) => {
   return reply.status(200).send({
     status: "ok",
     uptime: process.uptime(),
     timestamp: Date.now(),
+  });
+});
+
+fastify.get("/test", (request, reply) => {
+  const { userId } = getAuth(request);
+  if (!userId) {
+    return reply.status(401).send({ message: "Unauthorized" });
+  }
+  return reply.send({
+    message: "Order service is authenticated!",
   });
 });
 
